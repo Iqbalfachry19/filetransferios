cb3a0ae3b45fa5e8c436ac44c8d61915c1258ec4
iqbal <ifachry@rocketmail.com>
2023-08-01 20:37:56 +0700

    Initial Commit

---
+++ filetransfer/filetransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  filetransferApp.swift
+//  filetransfer
+//
+//  Created by Iqbal Fachry on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct filetransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
