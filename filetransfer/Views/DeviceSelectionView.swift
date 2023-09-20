be239d194c6ca570af5e0b1fce98c02811f0854f
iqbal <ifachry@rocketmail.com>
2023-09-20 21:36:12 +0700

    first commit

---
+++ filetransfer/Views/DeviceSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceSelectionView.swift
+//  filetransfer
+//
+//  Created by Iqbal Fachry on 02/08/23.
+//
+
+import SwiftUI
+
+struct DeviceSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeviceSelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeviceSelectionView()
+    }
+}
